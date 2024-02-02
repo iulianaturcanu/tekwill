c080e8ca045718d7911dc8fb21f70c21f7a563c1
iulianaturcanu <iuliana110@gmail.com>
2024-02-02 20:27:32 +0200

    Add materials for lesson #30

--- countries/script.js
+++ countries/script.js
@@ -18,12 +18,16 @@ const createCountryContainer = (country) => {
     return container;
 }
 
+let countriesList = [];
+
 const fetchData = () => {
     fetch('https://restcountries.com/v3.1/all')
     .then(response => response.json())
     .then(countries => {
+        countriesList = countries;
+        const countriesContainer = document.getElementById('countries');
+
         countries.forEach((country)=> {
-            const countriesContainer = document.getElementById('countries');
             const countryContainer = createCountryContainer(country);
             countriesContainer.appendChild(countryContainer);
         })
@@ -32,3 +36,86 @@ const fetchData = () => {
 }
 
 fetchData();
+
+const showAll =() => {
+    const countriesContainer = document.getElementById('countries');
+    countriesContainer.innerHTML = "";
+
+    const searchInput = document.getElementById('search-input');
+    searchInput.value = '';
+
+    fetchData();
+    handleInput();
+}
+
+const searchCountry = () => {
+    const searchInput = document.getElementById('search-input');
+    const countryName = searchInput.value.trim();
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then(response => response.json())
+    .then(countries => {
+        const countriesContainer = document.getElementById('countries');
+        countriesContainer.innerHTML = "";
+
+        countries.forEach((country)=> {
+            const countryContainer = createCountryContainer(country);
+            countriesContainer.appendChild(countryContainer);
+        })
+    })
+    .catch(error => {
+        const countriesContainer = document.getElementById('countries');
+        countriesContainer.innerHTML = '<div class="error">No results found.</div>'
+    });
+}
+
+const handleInput = () => {
+    const searchInput = document.getElementById('search-input');
+    const searchButton = document.getElementById('search-button');
+
+    searchButton.disabled = !searchInput.value.trim();
+}
+
+const searchInput = document.getElementById('search-input');
+searchInput.addEventListener('keydown', (event)=> {
+    if (event.key === 'Enter') {
+        searchCountry();
+    }
+})
+// searchButton.addEventListener('mouseover', () => {
+//     console.log('Mouse is over the button');
+// })
+
+handleInput()
+
+const array =  ['Ana', "Maria", "Albert"];
+array.sort((a, b) => a-b)
+// setTimeout(()=> console.log(countriesList), 1000);
+
+const sortCountries = (sortParam) => {
+    switch(sortParam) {
+        case 'sort':
+            return countriesList;
+        case 'population':
+            return countriesList.sort((a, b)=> a.population - b.population);
+        case 'area':
+            return countriesList.sort((a, b)=> a.area - b.area);
+        case 'name':
+        default:
+            return countriesList.sort((a, b)=> a.name.common.localeCompare(b.name.common));
+            
+    }
+}
+
+const sortSelect = document.getElementById('sort-select');
+sortSelect.addEventListener('change', ()=> {
+    const countriesContainer = document.getElementById('countries');
+    countriesContainer.innerHTML = '';
+
+    const sortedCountries = sortCountries(sortSelect.value);
+
+    sortedCountries.forEach((country)=> {
+        const countryContainer = createCountryContainer(country);
+        countriesContainer.appendChild(countryContainer);
+    })
+})
+
